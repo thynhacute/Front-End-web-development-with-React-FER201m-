33d69382e0a1886154c6f47536fc6f13260111f4
Hoàng Nhã Thy <77708167+thynhacute@users.noreply.github.com>
2022-07-18 23:02:52 +0700

    Add files via upload

---
+++ Lab5/src/components/DishDetailComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+import {
+    Media,
+    Card,
+    CardImg,
+    CardImgOverlay,
+    CardText,
+    CardBody,
+    CardTitle,
+} from "reactstrap";
+
+class DishDetail extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    renderDishDetail(){
+        
+    }
+}
+
+export default DishDetail;
